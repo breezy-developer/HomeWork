0bd2b8951ebe305e416b51ab8e9cc5a0a736b1b2
Arte <xistlaf@gmail.com>
2023-09-01 22:14:40 +0300

    Exircise1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+{
+    Console.Write("Введите первое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Ведите второе число: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int min;
+    int max;
+
+    if (a > b)
+    {
+        max = a;
+        min = b;
+
+    }
+    else
+    {
+        max = b; min = a;
+
+    }
+
+    Console.WriteLine("Максимальное число: {0}, Минимальное число:{1}", max, min);
+    Console.ReadLine();
+
+}
+
