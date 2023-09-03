92874fb88565faf924c3070bdfb38d356f058aea
Arte <xistlaf@gmail.com>
2023-09-03 12:37:57 +0300

    add Exircise2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNum =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNum;
+ if  (secondNum > max)
+{
+    max = secondNum;
+}
+ if (thirdNum > max)
+{
+    max = thirdNum;
+}
+Console.WriteLine("Максимальное число: {0}", max);
+Console.ReadLine();
