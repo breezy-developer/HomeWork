0eda4d9b6cba001fc1311bdb71cbf8e1fb6c9710
Arte <xistlaf@gmail.com>
2023-09-03 12:51:17 +0300

    add Exercise3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int numOddOrEven = Convert.ToInt32(Console.ReadLine());
+if  (numOddOrEven % 2 == 0)
+{
+    Console.WriteLine("Число {0} чётное", numOddOrEven);
+}
+else
+{
+    Console.WriteLine("Число {0} нечетное", numOddOrEven);
+}
+Console.ReadLine();
--- Exercise3/obj/Release/net7.0/Exercise3.AssemblyInfo.cs
+++ Exercise3/obj/Release/net7.0/Exercise3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exercise3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exercise3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exercise3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
