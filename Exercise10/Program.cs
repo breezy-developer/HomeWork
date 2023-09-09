61484ea0b12cf243bb28ca9c11bc216926b477fd
Arte <xistlaf@gmail.com>
2023-09-09 20:30:16 +0300

    Exercise 10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёх значное число: ");
+int threeDigitNumber = int.Parse(Console.ReadLine());
+string Number = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> " + Number[1]);
