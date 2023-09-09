0ace413134a45a7fba787ff69689f424fdb1f553
Arte <xistlaf@gmail.com>
2023-09-09 20:36:58 +0300

    Exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число");
+int numbers = int.Parse(Console.ReadLine());
+string numberText = Convert.ToString(numbers);
+if (numberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + numberText[2]);
+}
+else
+{
+    Console.Write("нет третьего числа");
+}
