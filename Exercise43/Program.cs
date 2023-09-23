0c0c32d5ceea2b628373c68287b8cd97d439cfc1
Arte <xistlaf@gmail.com>
2023-09-23 16:26:15 +0300

    Exercise 43

---
+++ Exercise43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
