7eb77056ec7244220584dfa4cbfb6f8f82f15abc
Arte <xistlaf@gmail.com>
2023-10-04 19:11:13 +0300

    Exercise 68

--- Exercise.64/Program.cs
+++ Exercise.64/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int Promt(string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{NaturalNumber(n, m + 1)} , ");
+    return m;
+}
+
+int n = Promt("Input N: ");
+if (n < 1)
+{
+    Console.WriteLine("Ввдите положительное число!");
+    return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
--- Exercise66/Program.cs
+++ Exercise66/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int SumOfElements(int n, int m)
+{
+    if (n == m) return n;
+    else return SumOfElements(n + 1, m) + n;
+}
+
+int n = Prompt("Input N: ");
+int m = Prompt("Input M: ");
+
+Console.WriteLine(SumOfElements(n, m));
