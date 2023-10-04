0d608713ddb01da33c1e45961ee576d5e3576dc1
Arte <xistlaf@gmail.com>
2023-10-04 19:02:03 +0300

    Exercise 64

---
+++ Exercise64/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int Promt(string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int NaturalNumber(int n , int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{NaturalNumber(n, m + 1)} , ");
+    return m;
+}
+
+int n = Promt("Input N: ");
+if (n < 1)
+{
+    Console.WriteLine("Ввдите положительное число!");
+    return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
